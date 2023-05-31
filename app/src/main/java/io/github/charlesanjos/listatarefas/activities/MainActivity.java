53ec3239d2786ed5716f849bbb3b2b0464ae697d
Charles Albert <charles.ccomp@gmail.com>
2023-05-31 08:48:54 -0300

    commit inicial, alteraçoes iniciais

---
+++ app/src/main/java/io/github/charlesanjos/listatarefas/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package io.github.charlesanjos.listatarefas.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.github.charlesanjos.listatarefas.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
